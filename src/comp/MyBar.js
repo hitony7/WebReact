3351be2e266a7c41c9b39f8e803275942122b46a
hitony7 <tywong11@gmail.com>
2020-03-08 04:14:56 -0600

    working logo button redirects

--- src/comp/MyBar.js
+++ src/comp/MyBar.js
@@ -6,14 +6,35 @@ import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 import Me from './imgs/me2.png'
+import gitLogo from './imgs/githubLogo.png'
+import linkLogo from './imgs/linkdphoto.png'
 /*
 Class componment for my nav bar
 */
+
+
 export class MyBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.linkndOnClick = this.linkndOnClick.bind(this);
+    this.gitOnClick = this.gitOnClick.bind(this);
+  }
+
+  linkndOnClick = () => {
+    console.log('this is:', this);
+    window.location = "https://www.linkedin.com/in/tony-wong-33713219a/"
+  }
+
+  gitOnClick = () => {
+    console.log('this is:', this);
+    window.location = "https://github.com/hitony7"
+  }
+
     render() {
         return (
             <div>
-  <Navbar bg="light" expand="lg">
+  <Navbar bg="light" variant="light" expand="lg">
   <Navbar.Brand href="#home">
       <img
         src= { Me }
@@ -37,6 +58,26 @@ export class MyBar extends Component {
         <NavDropdown.Item href="#action/3.4">Contact me</NavDropdown.Item>
       </NavDropdown>
     </Nav>
+    <Button variant="light" onClick ={ this.gitOnClick } >
+    <img
+        src= { gitLogo }
+        width="40"
+        height="40"
+        className="d-inline-block align-top"
+        alt="my face"
+        href="https://github.com/hitony7"
+      />
+    </Button>
+    <Button variant="light" onClick ={ this.linkndOnClick }>
+    <img
+        src= {linkLogo }
+        width="40"
+        height="40"
+        className="d-inline-block align-top"
+        alt="my face"
+        href="https://www.linkedin.com/in/tony-wong-33713219a/"
+      />
+    </Button>
     <Button variant="outline-success">Home</Button>
   </Navbar.Collapse>
 </Navbar>
