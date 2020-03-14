1aea0de4656189660889215df4d569564924c603
hitony7 <tywong11@gmail.com>
2020-03-14 14:43:13 -0600

    update

--- src/comp/TitlePage.js
+++ src/comp/TitlePage.js
@@ -1,7 +1,7 @@
 import Canvas from 'react-responsive-canvas';
 import React, { Component } from 'react'
 import Screen from './imgs/screen.png'
-import bitHi from './imgs/bitHi'
+import bitHi from './imgs/bitHi.png'
 import Image from 'react-bootstrap/Image'
 import GoogleFontLoader from 'react-google-font-loader';
 import { TaskTimer } from 'tasktimer';
@@ -62,41 +62,7 @@ updateDimensions() {
   console.log(window.innerHeight);
   this.setState({ heightSet });
 }
- /* componentDidMount() {
-    this.ctx = this.canvas.getContext('2d');
-    this.draw();
-}
-draw(){
-  const image = new Image();
-  image.src = Screen;
-  image.onload = () => {
-    this.ctx.fillStyle = '#393534';
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
-    console.log(this.canvas.height)
-  };
 
-}
-*/
-/*
-  componentDidMount() {
-    this.ctx = this.canvas.getContext('2d');
-    const img = this.refs.image
-    this.draw(img);
-    
-  }
- */
-/*
-  draw() {
-    // Draw whatever
-    this.ctx.drawImage(img, 0, 0)
-    this.ctx.globalCompositeOperation = 'destination-over';
-    this.ctx.fillStyle = 'rgba(0,255,0,1)';
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.fillStyle = "blue";
-    console.log("your adsamda");
-  }
-  */
   render () {
 
     const divStyle = {
@@ -183,11 +149,13 @@ draw(){
     const imageStyle = {
       width : "100%",
       height : "110% ",
-      zIndex: "-1",
-      postion : "absolute",
-      top: "500px",
+      zIndex: "-1",   
 
-      
+    }
+
+    const imageBitme = {
+      postion : "relative",
+      zIndex: "3",
 
     }
     return (
@@ -227,7 +195,7 @@ draw(){
                       src= { bitHi }
                       alt="bitme"
                       ref="image"
-                     
+                      style = {imageBitme}
                     />
       </div>
     );
