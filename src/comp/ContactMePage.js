54ff8f8c115260142cee45289f24b5f506e62a1c
Tony Wong <tywong11@gmail.com>
2020-05-05 03:01:13 -0600

    added contact me page

---
+++ src/comp/ContactMePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import MyBar from './MyBar'
+
+class ContactMePage extends Component {
+    render(){
+        return <div>
+            <MyBar/>
+            <h1>Contact me Page, What I have done, etc </h1>
+             </div>
+    }
+}
+
+export default ContactMePage
