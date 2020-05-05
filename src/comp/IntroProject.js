60425c199c66390cff8b74247b020efaaed8e0e3
Tony Wong <tywong11@gmail.com>
2020-05-05 01:04:12 -0600

    seng project add intro page

--- src/comp/IntroProject.js
+++ src/comp/IntroProject.js
@@ -32,48 +32,58 @@ export class IntroPage extends Component {
                     <Card>
                         <Card.Img variant="top" src={ErPic} />
                         <Card.Body>
-                            <Card.Title>Sample project</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Title>YouTube Playlist Course Compiler</Card.Title>
+                            <Card.Subtitle>Theme: University experience</Card.Subtitle>
+                             <br/>
+                             <Card.Subtitle className="mb-2 text-muted">Calgaryhacks 2020</Card.Subtitle>
                             <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
-                        </Card.Text>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>React</ListGroupItem>
-                            <ListGroupItem>Heroku</ListGroupItem>
-                            <ListGroupItem>JSX</ListGroupItem>
-                        </ListGroup>
-                        <Card.Body>
-                            <Card.Link href="#">Card Link</Card.Link>
-                            <Card.Link href="#">Another Link</Card.Link>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
+                            This project was made for UofC Calgaryhacks 2020 hosted by Computer science undergraduate society.
+                             We decide to create a website that goes through the class calendar to find all course at the university.
+                            Then it will create a youtube playlist from the keywords of the class calendar description, users will be able to rank the videos
+                            if they are relevant to the class they are taking.
+                            </Card.Text>    
+                            <br/>
+                            </Card.Body>
+                                <ListGroup className="list-group-flush">
+                                    <ListGroupItem>YouTube API</ListGroupItem>
+                                    <ListGroupItem>HTML, CSS, JQuery and Python</ListGroupItem>
+                                    <ListGroupItem>Database: Firestore</ListGroupItem>
+                                </ListGroup>
+                                <Card.Body>
+                                    <Card.Link href="https://github.com/justinf34/Hackathon2020">Github Link</Card.Link>
+                                    <Card.Link href="https://calgaryhacks-2020.devpost.com/">Calgaryhacks 2020 Link</Card.Link>
+                                    <Card.Link href="https://devpost.com/software/youtube-playlist-course-compiler-pbeosh">CH Project Link</Card.Link>
+                                </Card.Body>
+                            <Card.Footer>
+                                 <small className="text-muted">Jan 2020</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
                         <Card.Img variant="top" src={ErPic} />
                         <Card.Body>
-                            <Card.Title>Sample project</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Title>Academic Paper Journal System</Card.Title>
+                            <Card.Subtitle>Intro to Software Engineering </Card.Subtitle>
+                             <br/>
                             <Card.Text>
-                                This card has supporting text below as a natural lead-in to additional
-                            content.{' '}
-                            </Card.Text>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>Cras justo odio</ListGroupItem>
-                            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                        </ListGroup>
-                        <Card.Body>
-                            <Card.Link href="#">Github Link</Card.Link>
-                            <Card.Link href="#">Another Link</Card.Link>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
+                            This a system that allows user of many types user to interact with journals logging system. 
+                            This was for our software engineering course which focus on how to develop software with proper methodology/practices and agile development, which we used SCRUM.
+                            </Card.Text>    
+                            <br/>
+                            </Card.Body>
+                                <ListGroup className="list-group-flush">
+                                    <ListGroupItem>YouTube API</ListGroupItem>
+                                    <ListGroupItem>HTML, CSS, JQuery and Python</ListGroupItem>
+                                    <ListGroupItem>Database: Firestore</ListGroupItem>
+                                </ListGroup>
+                                <Card.Body>
+                                    <Card.Link href="https://github.com/justinf34/Hackathon2020">Github Link</Card.Link>
+                                    <Card.Link href="https://calgaryhacks-2020.devpost.com/">Calgaryhacks 2020 Link</Card.Link>
+                                    <Card.Link href="https://devpost.com/software/youtube-playlist-course-compiler-pbeosh">CH Project Link</Card.Link>
+                                </Card.Body>
+                            <Card.Footer>
+                                 <small className="text-muted">Jan 2020</small>
+                            </Card.Footer>
+                        
                     </Card>
                     <Card>
                         <Card.Img variant="top" src={ErPic} />
